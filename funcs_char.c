70471222f42196f3a7d3042306de564e6d3bdeca
ThibaudP <t.poncin@gmail.com>
2020-10-23 15:54:26 +0000

    Added functions store_char & store_string in funcs_char.c

---
+++ funcs_char.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * store_char - returns a single char to buffer
+ *
+ * @c: a char
+ *
+ * Return: a string
+ */
+
+char *store_char(va_list c)
+{
+	char ch = va_arg(c, int);
+	
+	return (ch);
+}
+
+/**
+ * store_string- returns a string to buffer
+ *
+ * @s: a string
+ *
+ * Return: a string
+ */
+
+void print_string(va_list str)
+{
+	char *arg = va_arg(str, char*);
+
+	if (arg == NULL)
+		arg = "(null)";
+
+	return(arg);
+}
