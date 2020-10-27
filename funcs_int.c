924c7aa17a661cb8468b56fd28e4813aea779ac7
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-10-27 08:49:04 +0000

    Add b option support

--- funcs_int.c
+++ funcs_int.c
@@ -36,9 +36,32 @@ pf_buf_t *store_int(va_list list, spec_data_t *data)
 
 pf_buf_t *store_binary(va_list list, spec_data_t *data)
 {
+	unsigned int nbr = va_arg(list, unsigned int);
 	pf_buf_t *tmp = NULL;
-	(void)list;
+	int length;
+	unsigned int i = 0, j = 0;
+	char *binary = malloc(sizeof(char *) * 32);
 	(void)data;
 
+	if (binary)
+	{
+		for (i = 1 << 31; i > 0; i = i / 2) 
+		{
+			binary[j] = (nbr & i) ? '1' : '0';
+			j++;
+		}
+
+		i = 0;
+		while (binary[i] == '0' && i < j - 1)
+			i++;
+		length = _strlen(binary + i);
+		if (length)
+		{
+			tmp = pf_buf_t_new(length);
+			if (tmp)
+				_strcpy(tmp->buf, binary + i);
+		}
+	}
+	free(binary);
 	return (tmp);
 }
