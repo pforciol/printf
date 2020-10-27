ea3f6dbba89b713c9597df38613b403eab7bcd41
ThibaudP <t.poncin@gmail.com>
2020-10-27 16:13:14 +0000

    Fix %p specifier for NULL pointer

--- funcs_hex.c
+++ funcs_hex.c
@@ -83,14 +83,10 @@ pf_buf_t *store_ptr(va_list list, spec_data_t *data)
 	void *ptr = va_arg(list, void*);
 	pf_buf_t *tmp = NULL;
 	int length;
-	char *str;
+	char *str = "";
 	(void)data;
 
-
-	if (str == NULL)
-		str = "(null)";
-	else
-		str = _ptrtohex(ptr);
+	str = _ptrtohex(ptr);
 
 	length = _strlen(str);
 	if (length)
